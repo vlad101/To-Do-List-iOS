372fe9deb1d0e8720c7bc1420549d736806ea9cb
vlad101 <vladimir.efros@gmail.com>
2013-07-09 09:18:17 -0400

    Added a new Font Category
    Added a new Font Category with the method that creates a new Font
    Object with the Action Man Font.

---
+++ To-Do-List/To-Do-List/UIFont+ListAdditions.h
@@ -0,0 +1,15 @@
+//
+//  UIFont+ListAdditions.h
+//  To-Do-List
+//
+//  Created by Vladimir Efros on 7/9/13.
+//  Copyright (c) 2013 Vladimir Efros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIFont (ListAdditions)
+
++ (id)ve_listFontWithSize:(CGFloat)size;
+
+@end
---
+++ To-Do-List/To-Do-List/UIFont+ListAdditions.m
@@ -0,0 +1,18 @@
+//
+//  UIFont+ListAdditions.m
+//  To-Do-List
+//
+//  Created by Vladimir Efros on 7/9/13.
+//  Copyright (c) 2013 Vladimir Efros. All rights reserved.
+//
+
+#import "UIFont+ListAdditions.h"
+
+@implementation UIFont (ListAdditions)
+
++ (id)ve_listFontWithSize:(CGFloat)size
+{
+    return [UIFont fontWithName:@"Action Man" size:size];
+}
+
+@end
